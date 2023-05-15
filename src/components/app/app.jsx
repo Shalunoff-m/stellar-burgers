8c81a93eeb94f043362cff19188b6902fdfd7258
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-05-15 07:56:06 +0300

    feat: + страница восстановления пароля

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,13 +6,14 @@ import { LayoutPage } from '../../pages/layout-page/layout-page';
 import { MainPage } from '../../pages/main-page/main-page';
 import { Login } from '../../pages/login/login';
 import { Register } from '../../pages/register/register';
+import { ForgotPassword } from '../../pages/forgot-password/forgot-password';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LayoutPage />}>
-          <Route index element={<Register />} />
+          <Route index element={<ForgotPassword />} />
         </Route>
       </Routes>
     </Router>
--- src/pages/forgot-password/forgot-password.jsx
+++ src/pages/forgot-password/forgot-password.jsx
@@ -2,13 +2,71 @@
 // pascalCase и kebabCase - модификаторы регистров
 import React from 'react';
 import styles from './forgot-password.module.css';
+import classNames from 'classnames';
+import {
+  Input,
+  Button,
+  PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
 function ForgotPassword() {
+  const [value, setValue] = React.useState('password');
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div>
-      Код ForgotPassword компонента
+    <div className={classNames(styles.box)}>
+      <p
+        className={classNames(
+          'text',
+          'text_type_main-medium',
+          styles.header,
+          'pb-6'
+        )}
+      >
+        Восстановление пароля
+      </p>
+      <Input
+        // className='m-6'
+        type={'text'}
+        placeholder={'Укажите e-mail'}
+        // onChange={(e) => setValue(e.target.value)}
+        // icon={'CurrencyIcon'}
+        // value={value}
+        name={'name'}
+        error={false}
+        // ref={inputRef}
+        // onIconClick={onIconClick}
+        errorText={'Ошибка'}
+        size={'default'}
+        extraClass='mb-6'
+      />
+
+      <Button htmlType='button' type='primary' size='medium' extraClass='mb-20'>
+        Восстановить
+      </Button>
+
+      <p className='p-0'>
+        <span
+          className={classNames(
+            'text',
+            'text_type_main-default',
+            'text_color_inactive',
+            'mr-2'
+          )}
+        >
+          Вспомнили пароль?
+        </span>
+        <a
+          href=''
+          className={classNames('text', 'text_type_main-default', styles.link)}
+        >
+          Войти
+        </a>
+      </p>
     </div>
   );
-};
+}
 
-export { ForgotPassword } ;
+export { ForgotPassword };
