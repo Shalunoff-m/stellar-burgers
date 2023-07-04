3dba7ca326315f9031ed7e4a9e25728722cf3d41
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-07-04 21:35:55 +0300

    feat: типизированы profile-link

--- src/components/profile-link/profile-link.tsx
+++ src/components/profile-link/profile-link.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
 import styles from './profile-link.module.css';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 
-function ProfileLink({ children, to, ...props }) {
-  const activeClass = ({ isActive }) => {
+interface IProfileLinkProps {
+  children: ReactNode;
+  to: string;
+  props: any;
+}
+
+type classNameProps = {
+  isActive: boolean;
+  isPending: boolean;
+};
+
+const ProfileLink: FC<IProfileLinkProps> = ({ children, to, ...props }) => {
+  const activeClass = ({ isActive }: classNameProps) => {
     if (isActive) {
       return classNames(
         'text',
@@ -29,6 +40,6 @@ function ProfileLink({ children, to, ...props }) {
       {children}
     </NavLink>
   );
-}
+};
 
 export { ProfileLink };
