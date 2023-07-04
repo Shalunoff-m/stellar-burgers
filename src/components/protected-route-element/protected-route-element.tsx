1810ced52edcd00ec7dcb8f5715e6809008fd18f
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-07-04 21:40:13 +0300

    feat: типизированы protected-route-element

--- src/components/protected-route-element/protected-route-element.tsx
+++ src/components/protected-route-element/protected-route-element.tsx
@@ -1,8 +1,15 @@
 import styles from './protected-route-element.module.css';
-import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
+import { FC, ReactElement, ReactNode } from 'react';
+import { useSelector } from '../../hooks/use-custom-redux';
 
-function ProtectedRouteElement({ element }) {
+interface IProtectedRouteElementProps {
+  element: ReactElement;
+}
+
+const ProtectedRouteElement: FC<IProtectedRouteElementProps> = ({
+  element,
+}) => {
   const location = useLocation();
   const { isAuthentificated } = useSelector((store) => store.user);
   // console.log(isAuthentificated);
@@ -12,6 +19,6 @@ function ProtectedRouteElement({ element }) {
   ) : (
     <Navigate to='/login' replace state={{ from: location.pathname }} />
   );
-}
+};
 
 export { ProtectedRouteElement };
